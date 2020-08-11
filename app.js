e108b023875e9b53651dc9a08d3446ed906965bd
Sophia <chiaoyin1520@gmail.com>
2020-08-11 13:27:13 +0800

    feat: add total time function

--- app.js
+++ app.js
@@ -2,28 +2,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const date = (req, res, next) => {
+  new Date() - new Date()
+  next()
+}
+
+
+
 app.use((req, res, next) => {
-  console.log('Time:', new Date(), '|', req.method, 'from', req.originalUrl)
+  console.log('Time1:', new Date(), '|', req.method, 'from', req.originalUrl, '|', 'total time:', new Date() - new Date(), 'ms')
+
+  next()
+})
+
+
+app.get('/', date, (req, res, next) => {
+  console.log('Time2:', new Date())
+  res.send('列出全部 Todo')
   next()
 })
 
 
 
-app.get('/', (req, res) => {
-
-  res.send('列出全部 Todo')
-})
-
 app.get('/new', (req, res) => {
   res.send('新增 Todo 頁面')
+
 })
 
 app.get('/:id', (req, res) => {
   res.send('顯示一筆 Todo')
+
 })
 
 app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
+
 })
 
 app.listen(port, () => {
